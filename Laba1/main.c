b92d1746c52d103dcf3754c2d735230c93d85346
Nikolay Savenko <savenkons@yandex.ru>
2020-10-03 01:51:54 +0300

    fuck this lab...

--- Laba1/main.c
+++ Laba1/main.c
@@ -1,50 +1,24 @@
 #include <stdio.h>
 #include <math.h>
-#define accuracy 0.00001
-
-float func(float x);
-int sign(float x);
-int hasNeededAccuracy(float bound, float root);
-float getRelativeRoot(float a, float b);
+#define e 0.00001
 
 int main()
 {
-    float lBorder, rBorder, relativeRoot;
-    int lBorderSign, rBorderSign;
-    scanf("%f %f", &lBorder, &rBorder);
-    relativeRoot  = getRelativeRoot(lBorder, rBorder);
-
-    while (!hasNeededAccuracy(func(lBorder), func(relativeRoot))) {
-        lBorderSign = sign(func(relativeRoot));
-        rBorderSign = sign(func(rBorder));
-        if (lBorderSign != rBorderSign) {
-            lBorder = relativeRoot;
+    float a, fa, b, c, fc;
+    scanf("%f %f", &a, &b);
+    c = (a + b) / 2;
+    while ((b - a) > e) {
+        fa = pow(2, a) - 2 * exp(-a);
+        fc = pow(2, c) - 2 * exp(-c);
+        if (fa * fc < 0) {
+            b = c;
         } else {
-            rBorder = relativeRoot;
+            a = c;
         }
-        relativeRoot = getRelativeRoot(lBorder, rBorder);
+        c = (a + b) / 2;
     }
 
-    printf("%f\n", relativeRoot);
+    printf("%f", c);
 
     return 0;
-}
-
-float func(float x) {
-    return pow(2, x) - 2 * exp(-x);
-}
-
-int hasNeededAccuracy(float bound, float root) {
-    return (root - bound) < accuracy;
-}
-
-float getRelativeRoot(float a, float b) {
-    return (a + b) / 2;
-}
-
-int sign(float x) {
-    int output = 0;
-    if (x > 0) output = 1;
-    if (x < 0) output = -1;
-    return output;
 }

b2beb85a97c2e2f83f860b628ead7ccb6434c724
Nikolay Savenko <savenkons@yandex.ru>
2020-11-26 20:40:14 +0300

    solution for 5 laba

---
+++ Laba5/main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define MAX_STRING_LEN 128
+
+int main() {
+	char splitters[MAX_STRING_LEN];
+	char string[MAX_STRING_LEN];
+	int i = 0;
+	int count = 0;
+	int neededLen;
+	int lastSplit = 0;
+	printf("[WARNING] max string len is %d\n", MAX_STRING_LEN);
+	printf("input splitters\n");
+	scanf("%s", &splitters);
+	printf("inputted splitters: %s\n\n", splitters);
+
+	printf("input string\n");
+	scanf("%s", &string);
+	printf("inputted string: %s\n\n", string);
+
+	printf("input needed len\n");
+	scanf("%d", &neededLen);
+
+	for (i = 0; i <= strlen(string); i++) {
+		if (strchr(splitters, string[i]) != NULL || string[i] == '\0') {
+			if (i - lastSplit == neededLen) {
+				count++;
+			}
+			lastSplit = i + 1;
+		}
+	}
+
+	printf("\nWords with len %d -> %d", neededLen, count);
+
+	return 0;
+}

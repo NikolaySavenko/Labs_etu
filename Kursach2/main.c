604511c6c08c3a137e22370dcb3d80672285940e
Nikolay Savenko <savenkons@yandex.ru>
2021-05-23 17:51:53 +0300

    advanced solution

--- Kursach2/main.c
+++ Kursach2/main.c
@@ -27,11 +27,11 @@ typedef struct DUCKDUCK {
 
 void show_info();
 
-int load_data(duck **ducksDest);
+int load_data(duck **ducksDest, char **filename);
+
+void save_data(duck **ducks, int count);
 
 int add_data(duck **ducks, int count);
-
-void sort_weight(struct DUCKDUCK *str0, int n);
 
 char **simple_split(char *str, int length, char sep);
 
@@ -41,17 +41,37 @@ int new_gets(char *s, int lim, char endSymbol);
 
 void show_data(duck **ducks, int count);
 
+void edit_data(duck **ducks, int count);
+
+int delete_data(duck **ducks, int count);
+
+void search_data(duck **ducks, int count);
+
+void sort_data(duck **ducks, int count);
+
+duck *get_new_duck();
+
+void free_duck(duck *target);
+
+void swap_data(duck **ducks, int count, int index0, int index1);
+
+void print_duck(duck *target_duck);
+
+void free_data(duck **ducks, int count);
+
 int main() {
 	duck *ducks = NULL;
+	char *filename = NULL;
 	int ducks_count;
-	char substring[MAX_LEN];
-	int success;
 	int state;
-	int choosen_type;
-	success = 0;
-	ducks_count = load_data(&ducks);
-	if (ducks_count >= 0) {
+
+	filename = (char *) malloc(MAX_LEN * sizeof (char));
+	strcpy(filename, "data.csv");
+	ducks_count = load_data(&ducks, &filename);
+	if (ducks_count >= 0 && filename != NULL) {
 		do {
+			printf("[DATABASE] %s\n", filename);
+			puts("");
 			puts("0: INFO");
 			puts("1: ADD");
 			puts("2: EDIT");
@@ -60,6 +80,10 @@ int main() {
 			puts("5: SEARCH");
 			puts("6: SORT");
 			puts("7: EXIT");
+			puts("---EXTRA---");
+			puts("10: load database from file");
+			puts("11: save database to file");
+			puts("");
 			scanf("%d", &state);
 
 			switch (state) {
@@ -76,43 +100,66 @@ int main() {
 				case 2:
 					CLS;
 					puts("EDIT");
-					getchar();
+					edit_data(&ducks, ducks_count);
 					break;
 				case 3:
 					puts("DELETE");
+					ducks_count = delete_data(&ducks, ducks_count);
 					getchar();
 					break;
 				case 4:
+					CLS;
 					puts("SHOW");
 					show_data(&ducks, ducks_count);
 					getchar();
 					break;
 				case 5:
+					CLS;
 					puts("SEARCH");
-					getchar();
+					search_data(&ducks, ducks_count);
 					break;
 				case 6:
+					CLS;
 					puts("SORT");
+					sort_data(&ducks, ducks_count);
 					getchar();
 					break;
 				case 7:
 					puts("EXIT");
+					break;
+				case 10:
+					CLS;
+					puts("READ");
+					printf("Input new database filename [MAX_LEN=%d]\n", MAX_LEN);
 					getchar();
+					new_gets(filename, MAX_LEN, '\n');
+					ducks_count = load_data(&ducks, &filename);
+					if (ducks_count > 0) {
+						puts("Success");
+					}
+					break;
+				case 11:
+					CLS;
+					puts("SAVE");
+					save_data(&ducks, ducks_count);
 					break;
 				default:
 					puts("Incorrect key!");
 					getchar();
 					break;
 			}
-			puts("Press ENTER to continue");
-			getchar();
-			CLS;
+			if (ducks_count < 0) {
+				state = 7;
+				puts("memory error occurs");
+			} else {
+				puts("Press ENTER to continue");
+				getchar();
+				CLS;
+			}
 		} while (state != 7);
 	} else puts("Error while file loading");
 
-	if (ducks != NULL) {
-		free(ducks);
-	}
+	free_data(&ducks, ducks_count);
 
 	return 0;
 }
@@ -165,19 +212,6 @@ char **simple_split(char *str, int length, char sep) {
 	return str_array;
 }
 
-void sort_weight(struct DUCKDUCK *str0, int n) {
-	struct DUCKDUCK tmp_struct;
-	int i, j;
-
-	for (i = 0; i < n; i++) {
-		tmp_struct = str0[i];
-		for (j = i - 1; j >= 0 && str0[j].weight > tmp_struct.weight; j--) {
-			str0[j + 1] = str0[j];
-		}
-		str0[j + 1] = tmp_struct;
-	}
-}
-
 int new_gets(char *s, int lim, char endSymbol) {
 	char c;
 	int i;
@@ -186,30 +220,11 @@ int new_gets(char *s, int lim, char endSymbol) {
 	return i;
 }
 
-int has_substring(char *str0, char *str1) {
-	char *str0_copy = (char *) malloc(sizeof(str0));
-	char *str1_copy = (char *) malloc(sizeof(str1));
-
-	int i, len0, len1;
-	len0 = (int) strlen(str0);
-	len1 = (int) strlen(str1);
-	strcpy(str0_copy, str0);
-	strcpy(str1_copy, str1);
-
-	for (i = 0; i < len0; i++) {
-		str0_copy[i] = (char) tolower(str0_copy[i]);
-	}
-	for (i = 0; i < len1; i++) {
-		str1_copy[i] = (char) tolower(str1_copy[i]);
-	}
-	return strstr(str0_copy, str1_copy) != NULL;
-}
-
 void show_info() {
 	puts("hehe");
 }
 
-int load_data(duck **ducksDest) {
+int load_data(duck **ducksDest, char **filename) {
 	char **s2 = NULL;
 	char *string = NULL;
 	char sep;
@@ -218,7 +233,7 @@ int load_data(duck **ducksDest) {
 	int slen, i, j;
 	int count = 0;
 	if ((string = (char *) malloc(MAX_LEN * sizeof(char) + 1)) != NULL) {
-		if ((df = fopen("data.csv", "r")) != NULL) {
+		if ((df = fopen(*filename, "r")) != NULL) {
 			sep = ';';
 			while (fgets(string, MAX_LEN, df)) {
 				count++;
@@ -274,64 +289,25 @@ int load_data(duck **ducksDest) {
 }
 
 int add_data(duck **ducks, int count) {
-	int symbolsCount;
-	char endSymbol;
-	int success;
-	success = 0;
+	duck *local = NULL;
 	getchar();
 	puts("Wants to add more?");
 	puts("1 - yes; any key - no; ");
 	while (getchar() == '1') {
 		getchar();
-		*ducks = (duck *) realloc(*ducks, (count + 1) * sizeof(duck));
-		puts("Input symbols count in name");
-		scanf("%d", &symbolsCount);
-		getchar();
-		(*ducks)[count].name = (char *) malloc(symbolsCount * sizeof(char));
-
-		puts("Input symbols count in type");
-		scanf("%d", &symbolsCount);
-		getchar();
-		(*ducks)[count].type = (char *) malloc(symbolsCount * sizeof(char));
-
-		if ((*ducks)[count].name == NULL || (*ducks)[count].type == NULL) {
-			success = 0;
-			puts("memory allocation error");
-		} else {
-			puts("Adding new duck with name:");
-			new_gets((*ducks)[count].name, MAX_LEN, '\n');
-
-			puts("With type:");
-			new_gets((*ducks)[count].type, MAX_LEN, '\n');
-
-			puts("Pos X of duck: ");
-			scanf("%d", &((*ducks)[count].position[0]));
-			puts("Pos Y of duck: ");
-			scanf("%d", &((*ducks)[count].position[1]));
-			puts("Weight of duck: ");
-			scanf("%f", &((*ducks)[count].weight));
-			puts("Height of duck: ");
-			scanf("%f", &((*ducks)[count].height));
-			puts("Paws count of duck: ");
-			scanf("%d", &((*ducks)[count].paws_count));
-			puts("Wings count of duck: ");
-			scanf("%d", &((*ducks)[count].wings_count));
-			getchar();
-
-			printf("name: %s type %s pos: %d:%d w: %f h: %f paws %d wings %d\n ",
-				   (*ducks)[count].name,
-				   (*ducks)[count].type,
-				   (*ducks)[count].position[0],
-				   (*ducks)[count].position[1],
-				   (*ducks)[count].weight,
-				   (*ducks)[count].height,
-				   (*ducks)[count].paws_count,
-				   (*ducks)[count].wings_count
-			);
-			count++;
-			puts("Wants to add more?");
-			puts("1 - yes; any key - no; ");
+		local = get_new_duck();
+		if (local != NULL) {
+			*ducks = (duck *) realloc(*ducks, (count + 1) * sizeof(duck));
+			if (*ducks != NULL) {
+				(*ducks)[count] = *local;
+				count++;
+			} else {
+				count = - 1;
+				/*it means error*/
+			}
 		}
+		puts("Wants to add more?");
+		puts("1 - yes; any key - no; ");
 	}
 	return count;
 }
@@ -339,7 +315,8 @@ int add_data(duck **ducks, int count) {
 void show_data(duck **ducks, int count) {
 	int i;
 	for (i = 0; i < count; i++) {
-		printf("name: %s type %s pos: %d:%d w: %f h: %f paws %d wings %d\n ",
+		printf("Duck# %d name: %s type %s pos: %d:%d w: %f h: %f paws %d wings %d\n ",
+			   i,
 			   (*ducks)[i].name,
 			   (*ducks)[i].type,
 			   (*ducks)[i].position[0],
@@ -352,3 +329,374 @@ void show_data(duck **ducks, int count) {
 	}
 }
 
+void edit_data(duck **ducks, int count) {
+	int choosen;
+	duck *new_duck = NULL;
+	show_data(ducks, count);
+	puts("Input duck index to edit:");
+	scanf("%d", &choosen);
+	if (choosen < count) {
+		new_duck = get_new_duck();
+		if (new_duck != NULL) {
+			(*ducks)[choosen] = *new_duck;
+			puts("Edited duck:");
+			print_duck(&(*ducks)[choosen]);
+		}
+	} else puts("i should be < count");
+}
+
+duck *get_new_duck() {
+	duck *new_duck = NULL;
+	int symbolsCount;
+	new_duck = (duck *) malloc(sizeof (duck));
+	if (new_duck != NULL) {
+		puts("Input symbols count in name");
+		scanf("%d", &symbolsCount);
+		getchar();
+
+		new_duck->name = (char *) malloc(symbolsCount * sizeof(char));
+
+		puts("Input symbols count in type");
+		scanf("%d", &symbolsCount);
+		getchar();
+		new_duck->type = (char *) malloc(symbolsCount * sizeof(char));
+
+		if (new_duck->name == NULL || new_duck->type == NULL) {
+			if (new_duck->name != NULL) free(new_duck->name);
+			if (new_duck->type != NULL) free(new_duck->type);
+			free(new_duck);
+			new_duck = NULL;
+			puts("memory allocation error");
+		} else {
+			puts("Adding new duck with name:");
+			new_gets(new_duck->name, MAX_LEN, '\n');
+
+			puts("With type:");
+			new_gets(new_duck->type, MAX_LEN, '\n');
+
+			puts("Pos X of duck: ");
+			scanf("%d", &new_duck->position[0]);
+			puts("Pos Y of duck: ");
+			scanf("%d", &new_duck->position[1]);
+			puts("Weight of duck: ");
+			scanf("%f", &new_duck->weight);
+			puts("Height of duck: ");
+			scanf("%f", &new_duck->height);
+			puts("Paws count of duck: ");
+			scanf("%d", &new_duck->paws_count);
+			puts("Wings count of duck: ");
+			scanf("%d", &new_duck->wings_count);
+			getchar();
+		}
+	} else {
+		puts("memory allocation error");
+		free(new_duck);
+		new_duck = NULL;
+	}
+	return new_duck;
+}
+
+void print_duck(duck *target_duck) {
+	printf("name: %s type %s pos: %d:%d w: %f h: %f paws %d wings %d\n ",
+		   target_duck->name,
+		   target_duck->type,
+		   target_duck->position[0],
+		   target_duck->position[1],
+		   target_duck->weight,
+		   target_duck->height,
+		   target_duck->paws_count,
+		   target_duck->wings_count
+	);
+}
+
+int delete_data(duck **ducks, int count) {
+	int choosen;
+	show_data(ducks, count);
+	puts("Input duck index to delete:");
+	scanf("%d", &choosen);
+	if (choosen < count) {
+		puts("Deleting duck...");
+		print_duck(&(*ducks)[choosen]);
+		while (choosen < count - 1) {
+			swap_data(ducks, count, choosen, choosen + 1);
+			choosen++;
+		}
+		free_duck(&(*ducks)[count - 1]);
+		count--;
+	} else puts("i should be < count");
+	return count;
+}
+
+void free_duck(duck *target) {
+	if (target != NULL) {
+		if (target->name != NULL) {
+			free(target->name);
+		}
+		if (target->type != NULL) {
+			free(target->type);
+		}
+	}
+}
+
+void swap_data(duck **ducks, int count, int index0, int index1) {
+	duck temp;
+	if (index0 < count && index1 < count) {
+		temp.name = (*ducks)[index0].name;
+		temp.type = (*ducks)[index0].type;
+		temp.position[0] = (*ducks)[index0].position[0];
+		temp.position[1] = (*ducks)[index0].position[1];
+		temp.weight = (*ducks)[index0].weight;
+		temp.height = (*ducks)[index0].height;
+		temp.paws_count = (*ducks)[index0].paws_count;
+		temp.wings_count = (*ducks)[index0].wings_count;
+
+		(*ducks)[index0].name = (*ducks)[index1].name;
+		(*ducks)[index0].type = (*ducks)[index1].type;
+		(*ducks)[index0].position[0] = (*ducks)[index1].position[0];
+		(*ducks)[index0].position[1] = (*ducks)[index1].position[1];
+		(*ducks)[index0].weight = (*ducks)[index1].weight;
+		(*ducks)[index0].height = (*ducks)[index1].height;
+		(*ducks)[index0].paws_count = (*ducks)[index1].paws_count;
+		(*ducks)[index0].wings_count = (*ducks)[index1].wings_count;
+
+		(*ducks)[index1].name = temp.name;
+		(*ducks)[index1].type = temp.type;
+		(*ducks)[index1].position[0] = temp.position[0];
+		(*ducks)[index1].position[1] = temp.position[1];
+		(*ducks)[index1].weight = temp.weight;
+		(*ducks)[index1].height = temp.height;
+		(*ducks)[index1].paws_count = temp.paws_count;
+		(*ducks)[index1].wings_count = temp.wings_count;
+	}
+}
+
+void search_data(duck **ducks, int count) {
+	int choosen;
+	int value_int;
+	float value_float;
+	int i;
+	char *string = NULL;
+	puts("Choose data type");
+	puts("--------------");
+	puts("0 - name");
+	puts("1 - type");
+	puts("2 - position_X");
+	puts("3 - position_Y");
+	puts("4 - paws_count");
+	puts("5 - wings_count");
+	puts("6 - weight");
+	puts("7 - height");
+	puts("any - exit");
+	puts("--------------");
+
+	scanf("%d", &choosen);
+	getchar();
+
+	string = (char *) malloc(MAX_LEN * sizeof (char));
+	if (choosen < 2) {
+		if (string != NULL) {
+			puts("Input string");
+			new_gets(string, MAX_LEN, '\n');
+		} else puts("Mem alloc error");
+	} else if (choosen < 6) {
+		puts("Input int value");
+		scanf("%d", &value_int);
+	} else if (choosen < 8) {
+		puts("Input float value");
+		scanf("%f", &value_float);
+	}
+	puts("");
+	for (i = 0; i < count; i++) {
+		if (choosen == 0 && strcmp(string, (*ducks)[i].name) == 0) {
+			print_duck(&(*ducks)[i]);
+		} else if (choosen == 1 && strcmp(string, (*ducks)[i].type) == 0) {
+			print_duck(&(*ducks)[i]);
+		} else if (choosen == 2 && (*ducks)[i].position[0] == value_int) {
+			print_duck(&(*ducks)[i]);
+		} else if (choosen == 3 && (*ducks)[i].position[1] == value_int) {
+			print_duck(&(*ducks)[i]);
+		} else if (choosen == 4 && (*ducks)[i].paws_count == value_int) {
+			print_duck(&(*ducks)[i]);
+		} else if (choosen == 5 && (*ducks)[i].wings_count == value_int) {
+			print_duck(&(*ducks)[i]);
+		} else if (choosen == 6 && (*ducks)[i].weight == value_float) {
+			print_duck(&(*ducks)[i]);
+		} else if (choosen == 7 && (*ducks)[i].height == value_float) {
+			print_duck(&(*ducks)[i]);
+		}
+	}
+	puts("");
+	puts("Done...");
+	if (choosen > 1 && choosen < 8) {
+		getchar();
+	}
+}
+
+void sort_data(duck **ducks, int count) {
+	int choosen, vector;
+	int i;
+	int step;
+	puts("Choose data type");
+	puts("--------------");
+	puts("0 - name");
+	puts("1 - type");
+	puts("2 - position_X");
+	puts("3 - position_Y");
+	puts("4 - paws_count");
+	puts("5 - wings_count");
+	puts("6 - weight");
+	puts("7 - height");
+	puts("any - exit");
+	puts("--------------");
+
+	scanf("%d", &choosen);
+
+	puts("Choose sort vector:");
+	puts("--------------");
+	puts("0 - down");
+	puts("1 - up");
+	puts("--------------");
+
+	scanf("%d", &vector);
+	puts("Sorting...");
+	for (step = 0; step < count - 1; ++step) {
+		for (i = 0; i < count - step - 1; ++i) {
+			if (choosen == 0) {
+				if (vector == 0 && strcmp((*ducks)[i].name, (*ducks)[i + 1].name) > 0) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				} else if (strcmp((*ducks)[i].name, (*ducks)[i + 1].name) < 0) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				}
+			} else if (choosen == 1) {
+				if (vector == 0 && strcmp((*ducks)[i].type, (*ducks)[i + 1].type) > 0) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				} else if (strcmp((*ducks)[i].type, (*ducks)[i + 1].type) < 0) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				}
+			} else if (choosen == 2) {
+				if (vector == 0 && (*ducks)[i].position[0] > (*ducks)[i + 1].position[0]) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				} else if ((*ducks)[i].position[0] < (*ducks)[i + 1].position[0]) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				}
+			} else if (choosen == 3) {
+				if (vector == 0 && (*ducks)[i].position[1] > (*ducks)[i + 1].position[1]) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				} else if ((*ducks)[i].position[1] < (*ducks)[i + 1].position[1]) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				}
+			} else if (choosen == 4) {
+				if (vector == 0 && (*ducks)[i].paws_count > (*ducks)[i + 1].paws_count) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				} else if ((*ducks)[i].paws_count < (*ducks)[i + 1].paws_count) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				}
+			} else if (choosen == 5) {
+				if (vector == 0 && (*ducks)[i].wings_count > (*ducks)[i + 1].wings_count) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				} else if ((*ducks)[i].wings_count < (*ducks)[i + 1].wings_count) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				}
+			} else if (choosen == 6) {
+				if (vector == 0 && (*ducks)[i].weight > (*ducks)[i + 1].weight) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				} else if ((*ducks)[i].weight < (*ducks)[i + 1].weight) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				}
+			} else if (choosen == 7) {
+				if (vector == 0 && (*ducks)[i].height > (*ducks)[i + 1].height) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				} else if ((*ducks)[i].height < (*ducks)[i + 1].height) {
+					swap_data(ducks, count, i, i + 1);
+					printf("%d<->%d\n", i, i + 1);
+				}
+			}
+		}
+	}
+	puts("Done");
+}
+
+void free_data(duck **ducks, int count) {
+	int i;
+	for (i = 0; i < count; i++) {
+		free_duck(&(*ducks)[i]);
+	}
+	if (*ducks != NULL) {
+		free(*ducks);
+	}
+}
+
+void save_data(duck **ducks, int count) {
+	char *filename = NULL;
+	char *buffer = NULL;
+	int buffer_len;
+	int i;
+	FILE *df;
+	filename = (char *) malloc(MAX_LEN * sizeof (char));
+	if (filename != NULL) {
+		printf("Input filename [MAX_LEN=%d]\n", MAX_LEN);
+		getchar();
+		new_gets(filename, MAX_LEN, '\n');
+		puts("");
+		df = fopen(filename, "w");
+		if (df != NULL) {
+			puts("Opened...");
+			for (i = 0; i < count; i++) {
+				printf("Writing duck #%d\n", i);
+				buffer_len = snprintf (NULL, 0, "%s;%s;%d;%d;%f;%f;%d;%d\n",
+						   (*ducks)[i].name,
+						   (*ducks)[i].type,
+						   (*ducks)[i].position[0],
+						   (*ducks)[i].position[1],
+						   (*ducks)[i].weight,
+						   (*ducks)[i].height,
+						   (*ducks)[i].paws_count,
+						   (*ducks)[i].wings_count);
+				buffer = (char *) malloc(buffer_len * sizeof (char));
+				if (buffer != NULL) {
+					snprintf (buffer, buffer_len + 1, "%s;%s;%d;%d;%f;%f;%d;%d\n",
+							  (*ducks)[i].name,
+							  (*ducks)[i].type,
+							  (*ducks)[i].position[0],
+							  (*ducks)[i].position[1],
+							  (*ducks)[i].weight,
+							  (*ducks)[i].height,
+							  (*ducks)[i].paws_count,
+							  (*ducks)[i].wings_count);
+					if (fputs(buffer, df) != EOF) {
+						puts("Success");
+					}
+					free(buffer);
+				} else {
+					puts("Error at memory allocation");
+				}
+			}
+			if (fclose(df) == EOF) {
+				puts("Error closing!");
+			} else {
+				puts("Closing...");
+			}
+		} else {
+			puts("Cannot create file");
+		}
+		puts("");
+		free(filename);
+	} else {
+		puts("Error at memory allocation");
+	}
+}

f6f384796f3c15269c704a0c9dbfdd1d8ca5eecd
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-11-01 17:56:07 +0300

    tests for size

--- Algos/Algos/Lab1_Tests/LinkedListTests.cs
+++ Algos/Algos/Lab1_Tests/LinkedListTests.cs
@@ -10,6 +10,18 @@ namespace Lab1_Tests
         {
             var list = new LinkedList<int>();
             Assert.Equal(0, list.get_size());
+
+            list.push_front(1);
+            Assert.Equal(1, list.get_size());
+
+            list.pop_front();
+            Assert.Equal(0, list.get_size());
+
+            list.push_back(1);
+            Assert.Equal(1, list.get_size());
+
+            list.pop_back();
+            Assert.Equal(0, list.get_size());
         }
     }
 }

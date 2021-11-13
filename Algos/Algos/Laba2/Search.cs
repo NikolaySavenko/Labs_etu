8b77a7bba8dccda0562017ba0fecf011fd0cd2f8
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-11-13 23:26:40 +0300

    binary search added

--- Algos/Algos/Laba2/Search.cs
+++ Algos/Algos/Laba2/Search.cs
@@ -2,9 +2,29 @@
 {
     public static class Search
     {
-        public static void BinarySearch<T>(T[] array)
+        // Extension Method
+        // For details read CLR via C# by Jeffrey Richter
+        public static int BinarySearch<T>(this T[] array, T value) where T : IComparable<T>
         {
-
+            int min = 0;
+            int max = array.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (value.CompareTo(array[mid]) == 0)
+                {
+                    return mid;
+                }
+                else if (value.CompareTo(array[mid]) < 0)
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
         }
     }
 }

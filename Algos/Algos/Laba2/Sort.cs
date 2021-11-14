068a6a51643d4029092e683658f10a7928e550ca
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-11-14 18:33:39 +0300

    quick sort realization

--- Algos/Algos/Laba2/Sort.cs
+++ Algos/Algos/Laba2/Sort.cs
@@ -8,15 +8,45 @@ namespace Laba2
 {
     public static class Sort
     {
-        public static void QuickSort<T>(T[] array)
+        static void Swap<T>(ref T x, ref T y)
         {
-
+            T t = x;
+            x = y;
+            y = t;
         }
 
-        public static void InsertionSort<T>(T[] array)
+        static int Partition<T>(T[] array, int minIndex, int maxIndex) where T : IComparable<T>
         {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i].CompareTo(array[maxIndex]) < 0)
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
 
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
         }
+
+        public static T[] QuickSort<T>(this T[] array, int minIndex, int maxIndex) where T : IComparable<T>
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivot = Partition(array, minIndex, maxIndex);
+            array.QuickSort(minIndex, pivot - 1);
+            array.QuickSort(pivot + 1, maxIndex);
+
+            return array;
+        }
+
+        
 
 
         public static void BogoSort<T>(T[] array)

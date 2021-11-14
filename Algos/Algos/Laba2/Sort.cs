aebced7b8f7043fe66ccd3f3cd38eb4c45fdce80
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-11-14 20:11:03 +0300

    counting sort

--- Algos/Algos/Laba2/Sort.cs
+++ Algos/Algos/Laba2/Sort.cs
@@ -97,9 +97,30 @@ namespace Laba2
             return array;
         }
 
-        public static void CountingSort(char[] array)
+        public static void CountingSort(this char[] array)
         {
+            int n = array.Length;
 
+            char[] output = new char[n];
+            int[] count = new int[256];
+
+            for (int i = 0; i < 256; ++i)
+                count[i] = 0;
+
+            for (int i = 0; i < n; ++i)
+                ++count[array[i]];
+
+            for (int i = 1; i <= 255; ++i)
+                count[i] += count[i - 1];
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                output[count[array[i]] - 1] = array[i];
+                --count[array[i]];
+            }
+
+            for (int i = 0; i < n; ++i)
+                array[i] = output[i];
         }
     }
 }

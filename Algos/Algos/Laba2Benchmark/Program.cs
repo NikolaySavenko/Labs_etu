3ba5fe5d15e4d7f969cb6b000a85aab6eb2893c4
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-11-16 08:03:23 +0300

    improved timer check

--- Algos/Algos/Laba2Benchmark/Program.cs
+++ Algos/Algos/Laba2Benchmark/Program.cs
@@ -11,6 +11,7 @@ var stopWatch = new Stopwatch();
 
 for (var arraySize = 10; arraySize < 1_000_000; arraySize *= 10)
 {
+    var middleTime = new TimeSpan();
     for (var i = 0; i < 10; i++)
     {
         var array = new int[arraySize];
@@ -24,14 +25,17 @@ for (var arraySize = 10; arraySize < 1_000_000; arraySize *= 10)
         array.QuickSort(0, array.Length - 1);
         stopWatch.Stop();
         Console.WriteLine($"[QuickSort{i}] Sorted! Ellapsed {stopWatch.Elapsed}");
+        middleTime += stopWatch.Elapsed;
         stopWatch.Reset();
         Console.WriteLine();
-    } 
+    }
+    Console.WriteLine($"\nMiddle Time [size={arraySize}]: {middleTime / 10}\n\n");
 }
 Console.WriteLine();
 
 for (var arraySize = 10; arraySize < 1_000_000; arraySize *= 10)
 {
+    var middleTime = new TimeSpan();
     for (var i = 0; i < 10; i++)
     {
         var array = new int[arraySize];
@@ -45,9 +49,11 @@ for (var arraySize = 10; arraySize < 1_000_000; arraySize *= 10)
         array.InsertionSort();
         stopWatch.Stop();
         Console.WriteLine($"[InsertionSort#{i}] Sorted! Ellapsed {stopWatch.Elapsed}");
+        middleTime += stopWatch.Elapsed;
         stopWatch.Reset();
         Console.WriteLine();
     }
+    Console.WriteLine($"Middle Time [size={arraySize}]: {middleTime / 10}\n\n");
 }
 Console.WriteLine("Finished!");
 Console.ReadKey();

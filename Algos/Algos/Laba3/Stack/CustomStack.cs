2a973193d38d710554f5b49ab8c0f25e6c73a1dc
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-12-04 12:56:26 +0300

    add custom stack

---
+++ Algos/Algos/Laba3/Stack/CustomStack.cs
@@ -0,0 +1,32 @@
+﻿namespace Laba3.Stack
+{
+    class CustomStack<T> where T : IComparable<T>
+    {
+        readonly Laba1.LinkedList<BinaryNode<T>> list;
+
+        public CustomStack()
+        {
+            list = new Laba1.LinkedList<BinaryNode<T>>();
+        }
+        public int Push(BinaryNode<T> element)
+        {
+            var len = list.Count;
+            list.push_back(element);
+            return len;
+        }
+        public BinaryNode<T>? Top => list.Count > 0 ? list.at(list.Count - 1) : null;
+        public void Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            list.remove(list.Count - 1);
+        }
+
+        public int Count()
+        {
+            return list.Count;
+        }
+    }
+}

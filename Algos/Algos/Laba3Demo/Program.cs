1a3c8c3ef92b1de5d2f77e096c000cd49f89660a
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-12-04 13:42:43 +0300

    add project for laba3 demo

---
+++ Algos/Algos/Laba3Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Laba3;
+
+var tree = new BinarySearchTree<int>();
+tree.Insert(8);
+tree.Insert(3);
+tree.Insert(10);
+tree.Insert(1);
+tree.Insert(6);
+tree.Insert(4);
+tree.Insert(7);
+tree.Insert(14);
+tree.Insert(13);
+var iterator = tree.CreateBFTEnumerator();
+while (iterator.MoveNext())
+{
+    Console.WriteLine(iterator.Current.Value);
+}

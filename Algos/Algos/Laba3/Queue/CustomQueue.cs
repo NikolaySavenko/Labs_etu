9749cd3852473b4f90259f8f23333ac8f4861a10
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-12-04 12:56:35 +0300

    add custom queue

---
+++ Algos/Algos/Laba3/Queue/CustomQueue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3.Queue
+{
+    class CustomQueue<T> where T : IComparable<T>
+    {
+        public T? Head { get; private set; }
+        readonly Laba1.LinkedList<T> list;
+
+        public CustomQueue()
+        {
+            list = new Laba1.LinkedList<T>();
+        }
+
+        public void Enqueue(T element)
+        {
+            list.push_front(element);
+        }
+
+        public T Dequeue()
+        {
+            return list.PopLastData();
+        }
+
+        public T Peek() => list.at(list.Count - 1);
+
+        public int Count => list.Count;
+    }
+}

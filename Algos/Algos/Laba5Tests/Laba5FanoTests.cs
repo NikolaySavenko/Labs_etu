97f5db3fc7e7001677dacc2c17e6cbdd0af44fbf
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2022-03-07 22:22:50 +0300

    cool fano tests

--- Algos/Algos/Laba5Tests/Laba5FanoTests.cs
+++ Algos/Algos/Laba5Tests/Laba5FanoTests.cs
@@ -6,10 +6,25 @@ namespace Laba5Tests;
 public class Laba5FanoTests
 {
     [Theory]
-    [InlineData("it is test string", "111 01 110 111 10 110 01 0011 10 01 110 10 01 0010 111 0001 0000")]
+    [InlineData("it is test string", "0111110101110011111100101110110111010001110000000")]
+    [InlineData("some another test", "01110110011101001001000110011100001110000001001111101011")]
     public void EncodeTest(string source, string encoded)
     {
         var encoder = new FanoEncoder(source);
-        Assert.Equal(encoded, encoder.ToString());
+        var actual = encoder.ToString();
+        Assert.Equal(encoded, actual);
+    }
+
+    [Theory]
+    [InlineData("ytttttttttttttttttttttttttttttttt")]
+    [InlineData("asdasdasdasdasdadsadasdasdasdasdasd")]
+    [InlineData("testtestttestetesttetstestestestestest")]
+    public void TestSize(string source)
+    {
+        var encoder = new FanoEncoder(source);
+        var sourceSizeInBytes = source.Length * 2;
+        var encodedSize = encoder.Encoded.ToArray().Length;
+        Assert.True(sourceSizeInBytes > encodedSize);
+
     }
 }

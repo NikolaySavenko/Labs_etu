869875676c02445313026c13f62c54221b43b574
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2022-04-17 21:54:50 +0300

    add 6 lab demo

---
+++ Algos/Algos/Lab6Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Lab6;
+
+var bellman = new BellmanFordOperator("./testData.csv");
+
+Console.WriteLine("Cost from:");
+var from = Console.ReadLine();
+
+Console.WriteLine("Cost to:");
+var to = Console.ReadLine();
+
+var cost = bellman.CalculateCost(from, to);
+
+Console.WriteLine($"Cost from {from} to {to} is {cost}");

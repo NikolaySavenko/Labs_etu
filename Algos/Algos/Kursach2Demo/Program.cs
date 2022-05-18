f7c49384daaebd27310384e81a697b1d480a34eb
Nikolay Savenko <savenkons@yandex.ru>
2022-05-18 18:08:16 +0300

    edmons karp demo

---
+++ Algos/Algos/Kursach2Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Kursach2;
+
+var edmonsKarp = new EdmonsKarp("./testData.csv");
+
+Console.WriteLine("Cost from:");
+var from = Console.ReadLine();
+
+Console.WriteLine("Cost to:");
+var to = Console.ReadLine();
+
+var cost = edmonsKarp.CalculateMaxFlow(from, to);
+
+Console.WriteLine($"Max Flow from {from} to {to} is {cost}");
